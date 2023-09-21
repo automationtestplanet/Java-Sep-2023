46e828a9199103f285bf831cf389178984317ef0
RAJU CHELLE <rajuchelle@gmail.com>
2023-09-21 10:06:05 +0530

    Revert "new java project"
    This reverts commit afdd56f04c67c3e3be5cd196b12ae5900bacb8f3.

--- Java-Sep-2023/src/demo/EnumExample.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public enum EnumExample {
-	
-	MONDAY;
-
-}
--- Java-Sep-2023/src/demo/InterfaceExample.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public interface InterfaceExample {
-	
-//	public static void main(String[] args) {
-//		System.out.println("Welcome to Interface");
-//	}
-
-}
