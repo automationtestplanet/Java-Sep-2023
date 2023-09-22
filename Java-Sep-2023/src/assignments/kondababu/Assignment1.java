6abe35dbf5bd71151c1c2c6969d1172cf9c0bf39
Katta Kondababu <kondababukatta@gmail.com>
2023-09-22 21:00:49 +0530

    new code added

--- Java-Sep-2023/src/assignments/kondababu/Assignment1.java
+++ Java-Sep-2023/src/assignments/kondababu/Assignment1.java
@@ -1,5 +1,32 @@
 package assignments.kondababu;
 
 public class Assignment1 {
-    // Write a program to print each word from the string ->  "Apple*Banana*Pomegranite*Watermelon"
+   
+	
+	
+	
+// Write a program to print each word from the string ->  "Apple*Banana*Pomegranite*Watermelon"
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String str= "Apple.Banana.Pomegranite.Watermelon";
+		String str1="\\.";
+		String str2[]=str.split(str1);
+		//System.out.println(str);
+		System.out.println(str2.length);
+		System.out.println("each words are:");
+		for (String each : str2) {
+			
+			
+			System.out.println(each);
+			
+		}
+			
+		}
+		
+		
+	
 }
+	
