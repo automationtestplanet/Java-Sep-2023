d5f49b118a87541042f32b6fb4fe4eb7237dfe9b
RAJU CHELLE <rajuchelle@gmail.com>
2023-09-22 09:59:22 +0530

    assignment1 to all

--- Java-Sep-2023/src/assignments/kalyan/Assignment1.java
+++ Java-Sep-2023/src/assignments/kalyan/Assignment1.java
@@ -1,5 +1,8 @@
 package assignments.kalyan;
 
 public class Assignment1 {
+	
+	//Write a program to display the sum of digits of a number 123456789
+	// output should be -> 1+2+3+4+5+6+7+8+9 = 45
 
 }
--- Java-Sep-2023/src/assignments/pavani/Assignment1.java
+++ Java-Sep-2023/src/assignments/pavani/Assignment1.java
@@ -2,4 +2,8 @@ package assignments.pavani;
 
 public class Assignment1 {
 
+	//Write a program to print even numbers between 1 to 10;
+	//Output should be - > 2,4,6,8,10
+	
+	
 }
