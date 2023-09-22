f3a1a10be2c97f159956d0c552d8cf282bd5aeec
SAI RAM <SAIRAM@example.com>
2023-09-22 19:31:04 +0530

    assignment1-sairam

--- Java-Sep-2023/src/assignments/sairam/Assignment1.java
+++ Java-Sep-2023/src/assignments/sairam/Assignment1.java
@@ -2,5 +2,21 @@ package assignments.sairam;
 
 public class Assignment1 {
 
-	//Write a program to covert a string "100" to Integer and convert the integer value to byte and convert the byte number to String 
+//Write a program to covert a string "100" to Integer and convert the integer value to byte and convert the byte number to String 
+public static void main(String[] args) {
+		
+		String string1 = "100"; 
+		
+		int int1 = Integer.parseInt(string1);
+		
+		System.out.println(int1);
+		
+
+		byte byte1 = 100;
+		
+		String string3 = String.valueOf(byte1);
+		System.out.println(string3);
 }
+}
+		
+		
