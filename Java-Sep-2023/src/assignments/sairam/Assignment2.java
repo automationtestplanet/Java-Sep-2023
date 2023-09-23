ea150cf3082a81fc39a300a0839b5c29d20a9cd7
SAI RAM <SAIRAM@example.com>
2023-09-23 13:07:46 +0530

    assignment2-sairam

--- Java-Sep-2023/src/assignments/sairam/Assignment2.java
+++ Java-Sep-2023/src/assignments/sairam/Assignment2.java
@@ -5,4 +5,10 @@ public class Assignment2 {
 	// Write a program to print the last 5 numbers in a reverse order for a given number
 	//	Ex: input 10,   o/p -> 10, 9, 8, 7 , 6
 	//	Ex: input 20,  o/p -> 20,19,18,17,16
+
+	public static void main(String[] args) {
+		
+	}
+
+
 }
