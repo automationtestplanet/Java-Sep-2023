638e430f811f42dc7fb2c5cfe89d526dc333eab2
Ravi kanakam <ravi.gkv306@gmail.com>
2023-09-23 14:17:01 +0530

    Code changes

--- Java-Sep-2023/src/assignments/ravikumar/Assignment1.java
+++ Java-Sep-2023/src/assignments/ravikumar/Assignment1.java
@@ -2,4 +2,13 @@ package assignments.ravikumar;
 
 public class Assignment1 {
     //Write a method to check whether the number is Even / Odd
+	
+	public static void main(String[] args) {
+		
+		int a;
+		int b;
+		System.out.println();
+	}
+	
+	
 }
