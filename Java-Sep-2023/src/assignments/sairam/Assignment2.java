bd9824c2b99dd56b74bd9f42486c94a67cfd7f4e
SAI RAM <SAIRAM@example.com>
2023-09-23 15:07:43 +0530

    assignment2-sairam

--- Java-Sep-2023/src/assignments/sairam/Assignment2.java
+++ Java-Sep-2023/src/assignments/sairam/Assignment2.java
@@ -5,10 +5,30 @@ public class Assignment2 {
 	// Write a program to print the last 5 numbers in a reverse order for a given number
 	//	Ex: input 10,   o/p -> 10, 9, 8, 7 , 6
 	//	Ex: input 20,  o/p -> 20,19,18,17,16
-
-	public static void main(String[] args) {
+   
+public static void main(String[] args) {
+	
+	int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+	int[] numbers2 = {11,12,13,14,15,16,17,18,19,20};
+	if(numbers.length < 5) {
+		System.out.println("No numbers are print");
 		
 	}
-
-
+	
+	for(int i = numbers.length - 1; i >= numbers.length - 5; i--) {
+			System.out.println(numbers[i]);
+			
+			
+	}
+	
+	if(numbers2.length < 5) {
+		System.out.println("No numbers are print");
+		
+	}
+	
+	for(int i1 = numbers2.length -1; i1 >= numbers2.length -5; i1--) {
+		System.out.println(numbers2[i1]);
+	}
+	
+}
 }
