dec7f92448f4c6820f4f3b0a774a77d7426e9613
RAJU CHELLE <rajuchelle@gmail.com>
2023-09-27 08:38:08 +0530

    PR review comments

--- Java-Sep-2023/src/assignments/kalyan/Assignment1.java
+++ Java-Sep-2023/src/assignments/kalyan/Assignment1.java
@@ -1,27 +1,21 @@
 package assignments.kalyan;
 
 public class Assignment1 {
-	
-	//Write a program to display the sum of digits of a number 123456789
-	// output should be -> 1+2+3+4+5+6+7+8+9 = 45
-	
-	public static void main(String[] args){
-	       String s1="123456789";
-	       int sum = 0;
-		   for(int i=0; i<s1.length(); i++)
-		
-		   {
-			   
-	             sum = sum+Character.getNumericValue(s1.charAt(i));
-	             System.out.print(s1.charAt(i));
-	             if(i<s1.length()-1) {
-	            	 System.out.print("+");
-	             }
-		   }
-		   
-	       		   System.out.print("=" + sum);
-	       		System.out.print(s1.length());
-		}
 
+	// Write a program to display the sum of digits of a number 123456789
+	// output should be -> 1+2+3+4+5+6+7+8+9 = 45
+	static int sumOfDigits(int input) {
+		String number = String.valueOf(input);
+		int result = 0;
+		for (int i = 0; i < number.length(); i++) {
+//			result = result + Character.getNumericValue(number.charAt(i));
+			result = result + number.charAt(i)-'0';
+		}
+		return result;
 	}
 
+	public static void main(String[] args) {
+		System.out.print(sumOfDigits(123456789));
+	}
+
+}
