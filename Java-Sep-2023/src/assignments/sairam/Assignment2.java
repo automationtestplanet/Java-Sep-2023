e5596fb4f7cc745d06bdcfae5f4805d9f07edac3
SAI RAM <SAIRAM@example.com>
2023-09-27 12:12:44 +0530

    assignment2-sairam

--- Java-Sep-2023/src/assignments/sairam/Assignment2.java
+++ Java-Sep-2023/src/assignments/sairam/Assignment2.java
@@ -10,6 +10,9 @@ public static void main(String[] args) {
 	
 	int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 	int[] numbers2 = {11,12,13,14,15,16,17,18,19,20};
+	int[] numbers3 = {12345};
+	
+	
 	if(numbers.length < 5) {
 		System.out.println("No numbers are print");
 		
