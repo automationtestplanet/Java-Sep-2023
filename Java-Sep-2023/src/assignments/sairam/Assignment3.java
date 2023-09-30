c5bfe85827b957f7a9d19679f671d487c010fb35
SAI RAM <SAIRAM@example.com>
2023-10-01 01:25:52 +0530

    assingment3-sairam resolve

--- Java-Sep-2023/src/assignments/sairam/Assignment3.java
+++ Java-Sep-2023/src/assignments/sairam/Assignment3.java
@@ -2,25 +2,23 @@ package assignments.sairam;
 
 public class Assignment3 {
 
-	// write a reusable method which should accept input as number and prints factorial value
+	// write a reusable method which should accept input as number and prints
+	// factorial value
 	// Ex: 5 -> 5*4*3*2*1 -> 120
-	// Ex: 4 -> 4*3*2*1  -> 25
-	
+	// Ex: 4 -> 4*3*2*1 -> 25
+
 	public void factorial(int n) {
 		int num = n;
-		int i;
 		int fact = 1;
-		for(i = 1; i<=n;i++) {
-			
-			fact =fact*i;
-			
+		for (int i = 1; i <= num; i++) {
+			fact = fact * i;
 		}
-		System.out.println("Factorial value is: "+ fact);
+		System.out.println("Factorial value is: " + fact);
 	}
-	
+
 	public static void main(String[] args) {
 		Assignment3 multi = new Assignment3();
-		
+
 		multi.factorial(5);
-   }
+	}
 }
