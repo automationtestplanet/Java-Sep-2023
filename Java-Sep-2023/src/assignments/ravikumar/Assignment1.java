260f50a5ab084414b7ebfb9f5af3d118ae24bf59
Ravi kanakam <ravi.gkv306@gmail.com>
2023-09-30 19:29:36 +0530

    code updated

--- Java-Sep-2023/src/assignments/ravikumar/Assignment1.java
+++ Java-Sep-2023/src/assignments/ravikumar/Assignment1.java
@@ -3,29 +3,23 @@ package assignments.ravikumar;
 import java.util.Scanner;
 
 public class Assignment1 {
-    //Write a method to check whether the number is Even / Odd
-	
-	
-	    public static void main(String[] args) {
-	           
-	    	int i = 3;
-            int k = 4;
-	        if (i % 2 == 0) {
-	            System.out.println(i + " is even.");
-	        } else {
-	            System.out.println(i + " is odd.");
-	        }
-	        
-	        if (k % 2 == 0) {
-	            System.out.println(k + " is even.");
-	        } else {
-	            System.out.println(k + " is odd.");
-	        }
+	// Write a method to check whether the number is Even / Odd
 
+	public static void main(String[] args) {
 
-	        
-	       
-	    }
+		int i = 3;
+		int k = 4;
+		if (i % 2 == 0) {
+			System.out.println(i + " is even.");
+		} else {
+			System.out.println(i + " is odd.");
+		}
+
+		if (k % 2 == 0) {
+			System.out.println(k + " is even.");
+		} else {
+			System.out.println(k + " is odd.");
+		}
+
 	}
-	
-
+}
