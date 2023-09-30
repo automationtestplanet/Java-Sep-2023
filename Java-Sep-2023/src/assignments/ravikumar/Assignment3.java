c60af8f34357713d1a98e8a987547dd18b93ae45
Ravi kanakam <ravi.gkv306@gmail.com>
2023-09-30 19:26:43 +0530

    Changes updated

--- Java-Sep-2023/src/assignments/ravikumar/Assignment3.java
+++ Java-Sep-2023/src/assignments/ravikumar/Assignment3.java
@@ -1,36 +1,28 @@
 package assignments.ravikumar;
+
 import java.util.Scanner;
+
 public class Assignment3 {
 
 //	Write a method which should accept and number as inut and check whether the number is Palindrome or not
 
- public static void main(String[] args) {
-	
-	 Scanner obj= new Scanner(System.in);
-	 System.out.print("Enter number: ");
-	 int num = obj.nextInt();
-	 int revNum= 0;
-	 int Onum = num;
-	 
-	 while(num !=0) {
-		 
-		 int rem = num %10;
-		 revNum = revNum*10 + rem;
-		 num = num /10;
-		 
-	 }
-	 
-	 if (Onum == revNum){
-		 System.out.println(Onum+"is a palidrome ");
-		 }
-	 else {
-		 System.out.println(Onum+"is not a palidrome ");
-	 }
-	 
-	 
-	 
-	 
-			 
+	public static void main(String[] args) {
+
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int num = obj.nextInt();
+		int revNum = 0;
+		int Onum = num;
+
+		while (num != 0) {
+			int rem = num % 10;
+			revNum = revNum * 10 + rem;
+			num = num / 10;
+		}
+		if (Onum == revNum) {
+			System.out.println(Onum + "is a palindrome ");
+		} else {
+			System.out.println(Onum + "is not a palindrome ");
+		}
+	}
 }
-}
-	
